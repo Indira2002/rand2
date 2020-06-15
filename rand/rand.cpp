63136d9a2ae345dd2be5096ee8990c860a4a7961
indira <indira.sultanova.2002@mail.ru>
2020-06-15 20:47:02 +0500

    commit rand

---
+++ rand/rand.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int Fill(int m[10][10])
+{
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			m[i][j] = rand();
+		}
+	}
+       return m[10][10];
+}
+	
+int main()
+{
+
+	srand(time(0));
+	int Matrixa[10][10];
+	Fill(Matrixa);
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			cout << Matrixa[i][j] << "\t";
+		}
+		cout << endl;
+	}
+	return 0;
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			if (Matrixa[i][j] % 2 == 0)
+			{
+				cout << 0 << endl;
+			}
+			else cout << Matrixa[i][j] << endl;
+		}
+		return 0;
+	}
+}
+
